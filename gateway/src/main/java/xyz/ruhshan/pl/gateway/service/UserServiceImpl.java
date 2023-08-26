212d2e474ce3a497147ea3ee464d80fe1f9e1695
Ruhshan <ruhshan.ahmed@gmail.com>
2023-08-26 23:57:15 +0600

    Add WebSecurityConfig in gateway

---
+++ gateway/src/main/java/xyz/ruhshan/pl/gateway/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package xyz.ruhshan.pl.gateway.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import xyz.ruhshan.pl.common.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findByEmail(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
