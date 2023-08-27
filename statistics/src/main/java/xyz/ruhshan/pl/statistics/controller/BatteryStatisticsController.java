09e7a26a35c8f64ff04e494ae25548df343cee7c
Ruhshan <ruhshan.ahmed@gmail.com>
2023-08-27 23:49:13 +0600

    Add battery statistics by post code range api in statistics module

---
+++ statistics/src/main/java/xyz/ruhshan/pl/statistics/controller/BatteryStatisticsController.java
@@ -0,0 +1,21 @@
+package xyz.ruhshan.pl.statistics.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.ruhshan.pl.common.dto.StatisticsResponseDto;
+import xyz.ruhshan.pl.statistics.service.BatteryStatisticsService;
+
+@RestController
+public class BatteryStatisticsController {
+    private final BatteryStatisticsService batteryStatisticsService;
+
+    public BatteryStatisticsController(BatteryStatisticsService batteryStatisticsService) {
+        this.batteryStatisticsService = batteryStatisticsService;
+    }
+
+    @GetMapping("/by-postcode-range/{start}/{end}")
+    public StatisticsResponseDto getStatisticsByPostCodeRange(@PathVariable Integer start, @PathVariable Integer end){
+        return batteryStatisticsService.getStatisticsByPostCodeRange(start, end);
+    }
+}
