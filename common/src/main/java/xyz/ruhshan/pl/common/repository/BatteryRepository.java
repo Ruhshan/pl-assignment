b8ddeb53176012ea00fc1bb34dd174ab019ee8b6
Ruhshan <ruhshan.ahmed@gmail.com>
2023-08-27 23:33:55 +0600

    Add service for statisitcs

--- common/src/main/java/xyz/ruhshan/pl/common/repository/BatteryRepository.java
+++ common/src/main/java/xyz/ruhshan/pl/common/repository/BatteryRepository.java
@@ -1,5 +1,6 @@
 package xyz.ruhshan.pl.common.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import xyz.ruhshan.pl.common.entity.BaseEntity;
@@ -7,5 +8,6 @@ import xyz.ruhshan.pl.common.entity.Battery;
 
 public interface BatteryRepository extends JpaRepository<BaseEntity, Long> {
     Optional<Battery> findByPostCodeAndName(Integer postCode, String name);
+    List<Battery> findAllByPostCodeBetween(Integer postCodeStart, Integer postCodeEnd);
 
 }
---
+++ statistics/src/main/java/xyz/ruhshan/pl/statistics/service/BatteryStatisticsServiceImpl.java
@@ -0,0 +1,38 @@
+package xyz.ruhshan.pl.statistics.service;
+
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import xyz.ruhshan.pl.common.dto.StatisticsResponseDto;
+import xyz.ruhshan.pl.common.entity.Battery;
+import xyz.ruhshan.pl.common.repository.BatteryRepository;
+
+@Service
+public class BatteryStatisticsServiceImpl implements BatteryStatisticsService {
+
+    private final BatteryRepository batteryRepository;
+
+    public BatteryStatisticsServiceImpl(BatteryRepository batteryRepository) {
+        this.batteryRepository = batteryRepository;
+    }
+
+
+    @Override
+    public StatisticsResponseDto getStatisticsByPostCodeRange(Integer postCodeStart, Integer postCodeEnd) {
+        Double totalWatts = 0.0;
+        List<String> names = new ArrayList<>();
+
+        List<Battery> batteryList =  batteryRepository.findAllByPostCodeBetween(postCodeStart, postCodeEnd);
+
+        for(Battery battery: batteryList){
+            totalWatts+=battery.getCapacity();
+            names.add(battery.getName());
+        }
+
+        Collections.sort(names);
+
+        return new StatisticsResponseDto(names, totalWatts);
+    }
+}
