30a99c4f4c6b1b8ec0f8ca1274885d2ac34ed107
Ruhshan <ruhshan.ahmed@gmail.com>
2023-08-27 21:04:10 +0600

    Add rabbitmq config

---
+++ common/src/main/java/xyz/ruhshan/pl/common/config/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package xyz.ruhshan.pl.common.config;
+
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    private final CachingConnectionFactory cachingConnectionFactory;
+
+    public RabbitMQConfig(CachingConnectionFactory cachingConnectionFactory) {
+        this.cachingConnectionFactory = cachingConnectionFactory;
+    }
+
+    @Bean
+    public Queue createUserRegistrationQueue() {
+        return new Queue("q.battery-registration");
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(Jackson2JsonMessageConverter converter) {
+        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory);
+        template.setMessageConverter(converter);
+        return template;
+    }
+
+}
