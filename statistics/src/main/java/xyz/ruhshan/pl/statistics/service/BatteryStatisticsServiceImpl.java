81eef10cd90875eeebbca060ffdac38f38b8f478
Ruhshan <ruhshan.ahmed@gmail.com>
2023-08-29 00:12:29 +0600

    Add battery statistics controller integration tests

--- statistics/src/main/java/xyz/ruhshan/pl/statistics/service/BatteryStatisticsServiceImpl.java
+++ statistics/src/main/java/xyz/ruhshan/pl/statistics/service/BatteryStatisticsServiceImpl.java
@@ -4,12 +4,17 @@ import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import xyz.ruhshan.pl.common.dto.StatisticsResponseDto;
 import xyz.ruhshan.pl.common.entity.Battery;
 import xyz.ruhshan.pl.common.repository.BatteryRepository;
 
 @Service
+@Slf4j
 public class BatteryStatisticsServiceImpl implements BatteryStatisticsService {
 
     private final BatteryRepository batteryRepository;
@@ -21,10 +26,26 @@ public class BatteryStatisticsServiceImpl implements BatteryStatisticsService {
 
     @Override
     public StatisticsResponseDto getStatisticsByPostCodeRange(Integer postCodeStart, Integer postCodeEnd) {
+
+        if(postCodeEnd < postCodeStart) {
+            var temp = postCodeEnd;
+            postCodeEnd = postCodeStart;
+            postCodeStart = temp;
+        }
+
+
+        log.info("Querying db for post code range {} to {}", postCodeStart, postCodeEnd);
+
+
         Double totalWatts = 0.0;
         List<String> names = new ArrayList<>();
 
         List<Battery> batteryList =  batteryRepository.findAllByPostCodeBetween(postCodeStart, postCodeEnd);
+
+        if(batteryList.isEmpty()) {
+            log.error("No battery exists for post code range {} to {}", postCodeStart, postCodeEnd);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No battery exists with given post code range");
+        }
 
         for(Battery battery: batteryList){
             totalWatts+=battery.getCapacity();
