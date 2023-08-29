2c781b95ac9ff7062978f5ef236de7b3230c488f
Ruhshan <ruhshan.ahmed@gmail.com>
2023-08-29 17:17:46 +0600

    Add aggregated statistics controller in gateway

--- gateway/src/main/java/xyz/ruhshan/pl/gateway/apiClient/StatisticsClient.java
+++ gateway/src/main/java/xyz/ruhshan/pl/gateway/apiClient/StatisticsClient.java
@@ -4,11 +4,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import xyz.ruhshan.pl.common.dto.RealtimeAggregatedStatDto;
 import xyz.ruhshan.pl.common.dto.StatisticsResponseDto;
 
 @FeignClient(name="statisticsClient", url="${statistics.url}")
 public interface StatisticsClient {
     @RequestMapping(method = RequestMethod.GET, value = "/by-postcode-range/{start}/{end}")
     StatisticsResponseDto getStatisticsByPostCodeRange(@PathVariable Integer start, @PathVariable Integer end);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/get-aggregated-stat")
+    RealtimeAggregatedStatDto getAggregatedStat();
 
 }
--- gateway/src/main/java/xyz/ruhshan/pl/gateway/controller/StatisticsController.java
+++ gateway/src/main/java/xyz/ruhshan/pl/gateway/controller/StatisticsController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.ruhshan.pl.common.dto.RealtimeAggregatedStatDto;
 import xyz.ruhshan.pl.common.dto.StatisticsResponseDto;
 import xyz.ruhshan.pl.gateway.service.StatisticsService;
 
@@ -20,6 +21,11 @@ public class StatisticsController extends ApiV1Controller{
     public StatisticsResponseDto getBatteryStatisticsByPostCodeRange(@PathVariable Integer postCodeStart, @PathVariable
         Integer postCodeEnd) {
         return statisticsService.getStatisticsByPostCodeRange(postCodeStart, postCodeEnd);
+    }
+
+    @GetMapping("/battery-statistics-aggregated")
+    public RealtimeAggregatedStatDto getAggregatedBatteryStatistics(){
+        return statisticsService.getAggregatedStatistics();
     }
 
 }
