6f6704de56d69a86ffe4611b43b656b6ce410b92
Ruhshan <ruhshan.ahmed@gmail.com>
2023-08-29 18:34:04 +0600

    Add more validation in battery registration request

--- common/src/main/java/xyz/ruhshan/pl/common/dto/BatteryRegistrationRequest.java
+++ common/src/main/java/xyz/ruhshan/pl/common/dto/BatteryRegistrationRequest.java
@@ -1,6 +1,9 @@
 package xyz.ruhshan.pl.common.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +13,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BatteryRegistrationRequest {
     @NotNull
+    @Size(min = 5, max = 50)
     private String name;
     @NotNull
+    @Min(10)
+    @Max(10000)
     private Integer postcode;
     @NotNull
+    @Min(0)
+    @Max(100000)
     private Double capacity;
 
 }
